8c5ea6f5473dc2d60cd14a251a6869d49d9d75cf
Juliano Costa Silva <apfjuliano@gmail.com>
2022-04-17 15:49:00 -0300

    feat: adding page-template and search

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Link from 'next/link'
 
-export const Header = () => {
+export function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <nav className='max-w-7xl mx-auto py-8 flex items-center justify-between'>
+        <div className='bg-gray-200'>[Logo]</div>
+        <ul className='flex'>
+          <li>
+            <Link href='/'>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/favoritos'>
+              <a>Favoritos</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
